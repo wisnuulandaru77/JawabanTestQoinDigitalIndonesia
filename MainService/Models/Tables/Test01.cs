1766c07d4550a1d8aa8087a722805ed0a24146c4
wisnu.ulandaru <wisnu.ulandaru@gmail.com>
2024-02-19 14:23:17 +0700

    update

--- MainService/Models/Tables/Test01.cs
+++ MainService/Models/Tables/Test01.cs
@@ -2,4 +2,13 @@
 
 public class Test01
 {
+    public int Id { get; set; }
+
+    public string Nama { get; set; }
+
+    public byte? Status { get; set; }
+
+    public DateTime? Created { get; set; }
+
+    public DateTime? Updated { get; set; }
 }
